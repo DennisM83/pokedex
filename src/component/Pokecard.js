67fedd1ea9520b1700dcd7a2782ecdd99d70ca39
Dennis Wenrich <dennisfinley@gmail.com>
2021-11-12 18:55:20 -0500

    added inline width to get the cards in a row

--- src/component/Pokecard.js
+++ src/component/Pokecard.js
@@ -5,7 +5,7 @@ const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprit
 export default function Pokecard(props) {
     let pic = `${POKE_API}${props.id}.png`
     return (
-        <div style={{ display: 'inline-block', marginRight: '30px'}}>
+        <div style={{ display: 'inline-block', margin: '0 auto', width: '20rem'}}>
         <div  style={{backgroundColor: '#fff', width: '300px', height: '400px', borderRadius: '10px', margin: '100px 140px', boxShadow: '5px 5px 10px #595959' }}>
             <h3 style={{ color: '#212121', paddingTop: '20px' }}>{props.name}</h3>
             <img src={pic} alt="pokemon charater"/>
