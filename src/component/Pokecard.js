fb5fe6cb8582b5a0ba8930e28f3de94eed385832
Dennis Wenrich <dennisfinley@gmail.com>
2021-11-13 11:50:21 -0500

    added the 'type' and 'base experience to cards

--- src/component/Pokecard.js
+++ src/component/Pokecard.js
@@ -9,6 +9,8 @@ export default function Pokecard(props) {
         <div  style={{backgroundColor: '#fff', width: '300px', height: '400px', borderRadius: '10px', margin: '100px 140px', boxShadow: '5px 5px 10px #595959' }}>
             <h3 style={{ color: '#212121', paddingTop: '20px' }}>{props.name}</h3>
             <img src={pic} alt="pokemon charater"/>
+            <p>{props.type}</p>
+            <p>{props.base}</p>
         </div>
         </div>
     )
